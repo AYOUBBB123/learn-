f88054383801500e43c15aceb8a7e6d20c26212f
Ayoubbezai2024 <bezaiayoub8@gmail.com>
2024-12-19 23:25:42 +0100

    5

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import Add from "./components/Add.tsx"
-import User from "./components/Users.tsx"
+import Users from './components/Users.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <div>
+  <StrictMode>
     <App />
-    <User />
- 
-    </div>
+    <Users/>
+  </StrictMode>,
 )
